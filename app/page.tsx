b7feebabb04bf3ae1d6860c4ee848ac306af1e7b
MCerqua <55824402+MCERQUA@users.noreply.github.com>
2025-07-14 23:44:16 -0400

    Add main page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { RubiksHouseScene } from '@/components/ui/rubiks-house';
+
+export default function Home() {
+  return (
+    <main>
+      <RubiksHouseScene />
+    </main>
+  );
+}
