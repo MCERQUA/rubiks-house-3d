941036be7c7ab52e73d44d6b16031d12d30ce8c3
MCerqua <55824402+MCERQUA@users.noreply.github.com>
2025-07-14 23:47:23 -0400

    Update Next.js config for Netlify static export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['three'],
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
